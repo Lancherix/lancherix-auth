252563ef1bdeeb2db5bd7f9f7819281460f0a5ae
lancherix <lancherix.service@gmail.com>
2026-08-13 20:02:43 -0500

    Card

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -41,6 +41,7 @@ const LoginPage = () => {
       const redirects = {
         studio: "https://studio.lancherix.com",
         labs: "https://labs.lancherix.com",
+        card: "http://localhost:3000",
       };
 
       const redirectBase = redirects[app];
